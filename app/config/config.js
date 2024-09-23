74a6f9cda4759dd1b76e1fff597e91262bbc1917
Reduwan Ahmed Khan <139320908+ReduwanKhan@users.noreply.github.com>
2024-09-23 21:16:07 +0600

    Project File Structure Completed

--- app/config/config.js
+++ app/config/config.js
@@ -2,3 +2,15 @@ export const PORT = 5050;
 export const DATABASE = "";
 export const JWT_KEY = "ABC12341241234";
 export const JWT_EXPIRE_TIME = 30 * 24 * 60 * 60;
+
+export const EMAIL_HOST = "";
+export const EMAIL_PORT = "";
+export const EMAIL_USER = "";
+export const EMAIL_PASS = "";
+
+export const WEB_CACHE = false;
+export const MAX_JSON_SIZE = "10MB";
+export const URL_ENCODE = true;
+
+export const REQUEST_TIME = 20 * 60 * 1000;
+export const REQUEST_NUMBER = 2000;
