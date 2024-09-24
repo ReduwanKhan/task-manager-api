44d474a16ccb6d44eed97f2f49c504737ff2f250
Reduwan Ahmed Khan <139320908+ReduwanKhan@users.noreply.github.com>
2024-09-24 16:18:53 +0600

    Route Config Done

--- app/controllers/TaskController.js
+++ app/controllers/TaskController.js
@@ -1,9 +1,19 @@
-export const CreateTask = async (req, res) => {};
+export const CreateTask = async (req, res) => {
+  return res.json({ status: "Success" });
+};
 
-export const UpdateTaskStatus = async (req, res) => {};
+export const UpdateTaskStatus = async (req, res) => {
+  return res.json({ status: "Success" });
+};
 
-export const TaskListByStatus = async (req, res) => {};
+export const TaskListByStatus = async (req, res) => {
+  return res.json({ status: "Success" });
+};
 
-export const DeleteTask = async (req, res) => {};
+export const DeleteTask = async (req, res) => {
+  return res.json({ status: "Success" });
+};
 
-export const CountTask = async (req, res) => {};
+export const CountTask = async (req, res) => {
+  return res.json({ status: "Success" });
+};
